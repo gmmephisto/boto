a300d2a750facc96e29b604288ce1627d86b529a
Anton Vazhnetsov <dragen15051@gmail.com>
2017-04-12 14:15:37 +0300

    ec2: add ExtNetwork entity and API methods in ec2.connection
    Signed-off-by: Anton Vazhnetsov <dragen15051@gmail.com>

---
+++ boto/ec2/extnetwork.py
@@ -0,0 +1,44 @@
+
+# Copyright (c) 2006-2011 Mitch Garnaat http://garnaat.org/
+# Copyright (c) 2011, Eucalyptus Systems, Inc.
+#
+# Permission is hereby granted, free of charge, to any person obtaining a
+# copy of this software and associated documentation files (the
+# "Software"), to deal in the Software without restriction, including
+# without limitation the rights to use, copy, modify, merge, publish, dis-
+# tribute, sublicense, and/or sell copies of the Software, and to permit
+# persons to whom the Software is furnished to do so, subject to the fol-
+# lowing conditions:
+#
+# The above copyright notice and this permission notice shall be included
+# in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+# OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABIL-
+# ITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT
+# SHALL THE AUTHOR BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+# WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+# IN THE SOFTWARE.
+"""
+Represents an EC2 External Networks
+"""
+from boto.ec2.ec2object import EC2Object
+
+
+class ExtNetwork(EC2Object):
+    def __init__(self, connection = None):
+        super(ExtNetwork, self).__init__(connection)
+        self.extnet_name = None
+        self.state = None
+        self.availability_zone = None
+
+    def endElement(self, name, value, connection):
+        if name == "extNetName":
+            self.extnet_name = value
+        elif name == "state":
+            self.state = value
+        elif name == 'availabilityZone':
+            self.availability_zone = value
+        else:
+            setattr(self, name, value)
